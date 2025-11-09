c0c860bf5d1b99de02a302fd87e887824fd67adf
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2025-11-09 18:52:10 +0100

    first programming tests

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
+signal curar_personaje()
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+#Contador 
+var tiempo_quieto = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,9 +20,31 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# Inicio animaciones
+# hamster en moviemnto
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("run")
+		if direction < 0:
+			$AnimatedSprite2D.flip_h = true # Animacion girada izquierda
+		else:
+			$AnimatedSprite2D.flip_h = false 
+		
+		tiempo_quieto = 0
+# hamster parado
 	else:
+		tiempo_quieto += delta #contador
+		
+		if tiempo_quieto > 3:
+			$AnimatedSprite2D.play("stop")
+		else:
+			$AnimatedSprite2D.play("idle")
+		
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+# FUNCION CURA 
+func curar():
+	curar_personaje.emit()
