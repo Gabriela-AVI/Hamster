db24d35becbd1d73dad07054d101d47c6089a165
Gabriela <gvazquezi24@fpcoruna.afundacion.org>
2025-11-10 23:32:30 +0100

    Pipe's movement updates

--- objects/pipe.gd
+++ objects/pipe.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
+# Start the pipe's movement
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimatedSprite2D.play("pipe")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
