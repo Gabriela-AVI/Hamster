0c06a6f49d6118ba7f3a902a0f49b4aa63c4461b
Gabriela <gvazquezi24@fpcoruna.afundacion.org>
2025-11-23 22:49:06 +0100

    Refactoring

--- menu/LevelController.gd
+++ menu/LevelController.gd
@@ -128,3 +128,5 @@ func finish_level():
 		print(" → NO UPDATE: fewer coins.")
 	print("==========================================================")
 	PlayersLoader.send_player_to_server()
+	
+	get_tree().change_scene_to_file("res://menu/MainMenu.tscn")
